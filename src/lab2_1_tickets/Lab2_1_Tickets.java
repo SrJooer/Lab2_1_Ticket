b0aea0a7fdc21f5edb883c1bc62ed539871baaef
Julio Torres <srjooer@gmail.com>
2026-07-25 08:30:18 -0600

    Commit inicial

---
+++ src/lab2_1_tickets/Lab2_1_Tickets.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab2_1_tickets;
+
+/**
+ *
+ * @author srjul
+ */
+public class Lab2_1_Tickets {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
