ca02bacb1f11d82f567fbbb1afd041a438e32305
duglo <doufy209@gmail.com>
2026-07-25 11:30:22 -0600

    hice el reset el dispatch y los objetos y el income

--- src/PalindromoAir.java
+++ src/PalindromoAir.java
@@ -52,10 +52,26 @@ public class PalindromoAir {
     }
 
     public double income(int index) {
+    if (index >= seats.length) {
         return 0.0;
     }
+    
+    double montoAsiento = 0.0;
+    
+    if (seats[index] != null) {
+        montoAsiento = seats[index].getMontof(); 
+    }
+    
+    return montoAsiento + income(index + 1);    }
 
     public void reset(int index) {
+    if (index >= seats.length) {
+        return;
+    }
+    
+    seats[index] = null;
+    
+    reset(index + 1);
     }
 
     public void sellTicket(String name) {
@@ -93,5 +109,12 @@ public class PalindromoAir {
     }
     
     public void dispatch() {
+    double total = income(0);
+    
+    System.out.println("Ingresos totales del vuelo: " + total);
+    
+    reset(0);
+    
+    System.out.println("El avión ha sido vaciado exitosamente.");
     }
 }
