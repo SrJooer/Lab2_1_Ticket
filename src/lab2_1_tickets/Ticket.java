893d23b6428f9f58b6b2b190cdb63e0c8e8c5650
Julio Torres <srjooer@gmail.com>
2026-07-25 08:45:36 -0600

    Clase Ticket.java

---
+++ src/lab2_1_tickets/Ticket.java
@@ -0,0 +1,44 @@
+
+package ticket;
+
+
+public class Ticket {
+   private String Nombre;
+   private double Montof;
+   private double monto;
+   private boolean Palindromo;
+
+    public Ticket(String Nombre, double Montof, double monto, boolean Palindromo) {
+        this.Nombre = Nombre;
+        this.Montof = Montof;
+        this.monto = monto;
+        this.Palindromo = Palindromo;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public double getMontof() {
+        return Montof;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public boolean isPalindromo() {
+        return Palindromo;
+    }
+    public void print() {
+        System.out.println("DATOS DEL TICKET:");
+        System.out.println("Pasajero: " + Nombre);
+        System.out.println("Monto Original: $" + monto);
+        System.out.println("Monto Pagado: $" + Montof);
+      if (Palindromo) {
+        System.out.println("Si aplica descuento!");
+    } else {
+        System.out.println("No aplica descuento");
+    }    }
+        
+}
